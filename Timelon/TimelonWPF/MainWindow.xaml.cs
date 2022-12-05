dc81d944d7accdf2df1dd016d76a04893b192b0a
PolShestogo <99017989+PolShestogo@users.noreply.github.com>
2022-12-05 19:17:12 +0300

    NerebotautButton

--- Timelon/TimelonWPF/MainWindow.xaml.cs
+++ Timelon/TimelonWPF/MainWindow.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Windows;
 using System.Windows.Controls;
+using TimelonWPF;
 
 namespace TimelonWPF
 {
@@ -152,5 +153,11 @@ namespace TimelonWPF
         }
 
         #endregion Window Manager Events
+
+        private void CompleteStatus_MouseDown(object sender, System.Windows.Input.MouseButtonEventArgs e)
+        {
+            
+        }
+
     }
 }
