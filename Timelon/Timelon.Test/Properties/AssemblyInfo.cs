b9439d7fe0ed3c601bae77717e8505231625bd72
dubstepTractor <106086600+dubstepTractor@users.noreply.github.com>
2022-12-11 22:05:37 +0300

    update assembly info

--- Timelon/Timelon.Test/Properties/AssemblyInfo.cs
+++ Timelon/Timelon.Test/Properties/AssemblyInfo.cs
@@ -3,11 +3,11 @@ using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 [assembly: AssemblyTitle("Timelon.Test")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Unit test for library and application")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Arkham")]
 [assembly: AssemblyProduct("Timelon.Test")]
-[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyCopyright("Copyright © Arkham 2022")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
